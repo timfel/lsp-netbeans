d0871b2140809a69cf81058b15847666026e0698
Tim Felgentreff <tim.felgentreff@oracle.com>
2021-10-07 15:12:39 +0200

    Release 0.0.1

--- lsp-netbeans-pkg.el
+++ lsp-netbeans-pkg.el
@@ -1,4 +1,4 @@
 (define-package
   "lsp-netbeans"
-  "VERSION"
+  "0.0.1"
   "A package to use the Netbeans based LSP and DAP server with emacs-lsp.")
--- lsp-netbeans.el
+++ lsp-netbeans.el
@@ -1,6 +1,6 @@
 ;;; lsp-netbeans.el --- Apache Netbeans Language Server Client settings
 
-;; Version: VERSION
+;; Version: 0.0.1
 
 ;; Author: Tim Felgentreff <timfelgentreff@gmail.com>
 ;; Package-Requires: ((emacs "25.1") (lsp-mode "6.0") (markdown-mode "2.3") (dash "2.18.0") (f "0.20.0") (ht "2.0") (request "0.3.0") (treemacs "2.5") (dap-mode "0.5"))
